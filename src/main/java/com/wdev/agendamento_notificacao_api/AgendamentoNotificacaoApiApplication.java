ee5bdc0118114cbbce22e79d1fa8944c699aa0a2
Wdev <wrlandev0@gmail.com>
2025-02-26 18:40:17 -0300

    First commit

---
+++ src/main/java/com/wdev/agendamento_notificacao_api/AgendamentoNotificacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.wdev.agendamento_notificacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoNotificacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoNotificacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdev/agendamento_notificacao_api/AgendamentoNotificacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdev.agendamento_notificacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoNotificacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
