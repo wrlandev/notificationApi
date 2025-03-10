7b0e38df9ccd6a999c743afe0d87bb7b5fb848ba
Wdev <wrlandev0@gmail.com>
2025-03-10 13:26:32 -0300

    feat:add agendamento service

---
+++ src/main/java/com/wdev/agendamento_notificacao_api/services/AgendamentoService.java
@@ -0,0 +1,21 @@
+package com.wdev.agendamento_notificacao_api.services;
+
+import com.wdev.agendamento_notificacao_api.controller.dto.AgendamentoRecord;
+import com.wdev.agendamento_notificacao_api.controller.dto.AgendamentoRecordOut;
+import com.wdev.agendamento_notificacao_api.infra.repositories.AgendamentoRepository;
+import com.wdev.agendamento_notificacao_api.services.mapper.IAgendamentoMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AgendamentoService {
+
+    private final AgendamentoRepository repository;
+    private final IAgendamentoMapper agendamentoMapper;
+
+    public AgendamentoRecordOut gravarAgendamento(AgendamentoRecord agendamento) {
+        return agendamentoMapper.paraOut(repository.save(agendamentoMapper.paraEntity(agendamento)));
+    }
+
+}
